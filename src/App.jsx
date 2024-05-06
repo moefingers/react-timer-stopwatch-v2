ae251599222fc199b118865145459f9af24a4d7c
MoeFingers <mbzuiter@gmail.com>
2024-05-05 18:03:39 -0600

    added a child that animates position on click with the circlization

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [testClassNameActive, setTestClassNameActive] = useState(false)
+  const [circlizedActive, setCirclizedActive] = useState(false)
 
   return (
     <>
-      <div className={`test ${testClassNameActive ? 'testActive' : ''}`} onClick={() => setTestClassNameActive(!testClassNameActive)}>suh</div>
+      <div className={`test-wrapper ${circlizedActive ? 'test-wrapper-altered' : ''}`} onClick={() => setCirclizedActive(!circlizedActive)}>
+        <div className={`test-child ${circlizedActive ? 'test-child-altered' : ''}`}>5:00</div>
+      </div>
     </>
   )
 }
